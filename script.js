ff74c78589aac1085f18b3da4131a573f8d6e876
U123699 <3900320865@qq.com>
2025-12-16 16:07:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ const resourcesData = [
         level: "hsk1",
         type: "textbook",
         description: "HSK一级标准教材，适合零起点学习者 | HSK Level 1 standard textbook for complete beginners",
-        fileSize: "25.4 MB",
+        fileSize: "13 MB",
         fileType: "PDF",
         downloadUrl: "https://raw.githubusercontent.com/U123699/chinese-learning-resources/main/hsk1/hsk1-textbook.pdf",
         githubUrl: "https://github.com/U123699/chinese-learning-resources/blob/main/hsk1/hsk1-textbook.pdf"
     }
+    {
+    id: 2,
+    title: "HSK1练习册 | HSK 1 Workbook",
+    author: "姜丽萍 | Jiang Liping",
+    level: "hsk1",
+    type: "workbook",
+    description: "HSK一级练习册 | HSK Level 1 workbook",
+    fileSize: "23 MB", // 修改为实际大小
+    fileType: "PDF",
+    downloadUrl: "https://raw.githubusercontent.com/U123699/chinese-learning-resources/main/hsk1/hsk1-workbook.pdf",
+    githubUrl: "https://github.com/U123699/chinese-learning-resources/blob/main/hsk1/hsk1-workbook.pdf"
+}
     // 当你上传更多文件后，在这里按照格式添加
     // When you upload more files, add them here in the same format
     // 注意：id 数字要递增，不要重复
@@ -169,3 +181,4 @@ async function loadResourcesFromGitHub() {
     // 需要一个 resources.json 文件来存储资源信息
     // Need a resources.json file to store resource information
 }
+
